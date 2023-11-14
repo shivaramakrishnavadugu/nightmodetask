3055636ee945a51140530d81f6156b764c31f5d6
Shivaramakrishna Vadugu <140233005+shivaramakrishnavadugu@users.noreply.github.com>
2023-11-14 15:00:27 +0530

    night mode on off task

---
+++ script.js
@@ -0,0 +1,12 @@
+    document.getElementById("on").addEventListener("click", function(){
+        document.getElementById("body").style.backgroundColor="black";
+        document.getElementById("body").style.color = "grey";
+    });
+
+    document.getElementById("off").addEventListener("click", function(){
+        document.getElementById("body").style.backgroundColor="white";
+        document.getElementById("body").style.color = "black";
+    });
+
+        
+  
